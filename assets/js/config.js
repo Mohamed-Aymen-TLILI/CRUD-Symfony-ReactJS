4ba575bebcfc9fe4b03ddab3833de00a8ba26ac6
libra1919 <aymentli@gmail.com>
2020-05-14 20:56:04 +0200

    App first curd

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+//
+
+export const CUSTOMERS_API = "http://127.0.0.1:8000/api/customers";
+export const INVOICES_API =  "http://127.0.0.1:8000/api/invoices";
+export const USERS_API =  "http://127.0.0.1:8000/api/users";
+export const LOGIN_API = "http://127.0.0.1:8000/api/login_check";
---
+++ assets/js/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const HomePage = props => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">My@ppwebpro</h1>
+      <p className="lead">
+        www.myappwebpro.com
+      </p>
+      <hr className="my-4" />
+      <p>
+        My@ppwebpro
+      </p>
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Learn more
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
